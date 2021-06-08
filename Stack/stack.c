027662df1b9a58ad8631a05e7c7190f438d042c0
Harshul Sahni <hsahni@morsecorp.com>
2021-06-08 16:35:20 -0400

    Add foundation for stack implementation

---
+++ Stack/stack.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "stack.h"
+
+struct Stack *make_stack() {
+    struct Stack *stack = malloc(sizeof(struct Stack)); 
+}
---
+++ Stack/stack.h
@@ -0,0 +1,10 @@
+#ifndef STACK_SEEN
+#define STACK_SEEN
+#include "../LinkedList/linked_list.h"
+
+struct Stack {
+    int size; 
+    struct LinkedList *list; 
+}
+
+#endif
